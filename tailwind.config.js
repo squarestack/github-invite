482580dcc989ce6fe2e4e6698b317dc362017a92
Igor Kowalczyk <majonezexe@protonmail.com>
2022-08-31 09:05:03 +0000

    Fix tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
- content: ["./src/**/*.{html,js}"],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
  theme: {
    extend: {},
  },
- plugins: [],
+ plugins: [
+  require("@headlessui/tailwindcss"),
+  require("@igorkowalczyk/is-browser"),
+  require("@tailwindcss/aspect-ratio"),
+ ],
 }
