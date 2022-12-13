fff112d84993ea2cdd76502253a45b605a916e4b
Igor Kowalczyk <majonezexe@protonmail.com>
2022-12-13 19:08:30 +0100

    Add favicons

--- components/Container.jsx
+++ components/Container.jsx
@@ -6,6 +6,7 @@ export function Container(props) {
  const { children, ...customMeta } = props;
  const router = useRouter();
  const meta = {
+  url: "https://invite.squarestack.vercel.app",
   ...customMeta,
  };
 
@@ -16,6 +17,12 @@ export function Container(props) {
     <meta content={meta.description} name="description" />
     <meta property="og:url" content={meta.url + router.asPath} />
     <link rel="canonical" href={meta.url + router.asPath} />
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+    <link rel="manifest" href="/favicons/site.webmanifest" />
+    <meta name="msapplication-TileColor" content="#000000" />
+    <meta name="theme-color" content="#000000" />
    </Head>
    <Nav />
    <div className="flex min-h-screen flex-col bg-[url('/background.png')] bg-cover bg-fixed bg-no-repeat antialiased">{children}</div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -42,7 +42,7 @@ export default function Home() {
  };
 
  return (
-  <Container>
+  <Container title="Square Stack - Join">
    <div className="flex min-h-screen flex-col items-center justify-center py-2 duration-200">
     <Link href={`https://github.com/` + org} className="text-2xl font-bold leading-tight tracking-tighter text-white">
      <div className="flex items-center justify-center gap-4 text-5xl">
