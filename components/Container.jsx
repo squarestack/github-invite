c86127f941bcdd49f52814f25e62ab50481a1b0c
Igor Kowalczyk <majonezexe@protonmail.com>
2022-12-22 12:45:38 +0100

    Update favicons in code

--- components/Container.jsx
+++ components/Container.jsx
@@ -17,12 +17,25 @@ export function Container(props) {
     <meta content={meta.description} name="description" />
     <meta property="og:url" content={meta.url + router.asPath} />
     <link rel="canonical" href={meta.url + router.asPath} />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Square Stack" />
+    <meta property="og:description" content={meta.description} />
+    <meta property="og:title" content={meta.title} />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@stack_square" />
+    <meta name="twitter:title" content={meta.title} />
+    <meta name="twitter:description" content={meta.description} />
+    <meta name="twitter:creator" content="@stack_square" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
     <link rel="manifest" href="/favicons/site.webmanifest" />
+    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
+    <link rel="shortcut icon" href="/favicons/favicon.ico" />
     <meta name="msapplication-TileColor" content="#000000" />
-    <meta name="theme-color" content="#000000" />
+    <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+    <meta name="theme-color" content="#000000"></meta>
    </Head>
    <Nav />
    <div className="flex min-h-screen flex-col bg-[url('/background.png')] bg-cover bg-fixed bg-no-repeat antialiased">{children}</div>
