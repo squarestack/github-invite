c0dde3d9967fac2be10c2b4c9aba4a803e52d974
Igor Kowalczyk <majonezexe@protonmail.com>
2023-10-24 12:41:29 +0200

    Update @igorkowalczyk/eslint-config to v2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
- extends: ["@igorkowalczyk/eslint-config", "next"],
+ extends: ["@igorkowalczyk/eslint-config/legacy", "next"],
  rules: {
   "react/no-unescaped-entities": "off",
   "react/prop-types": "off",
+  "@next/next/no-html-link-for-pages": "off",
+  "import/no-anonymous-default-export": "off",
+ },
+ env: {
+  es6: true,
  },
 };
