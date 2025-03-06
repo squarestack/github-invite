ce93d1f0b90db277148ffc075b3ffa2fddc601db
Igor Kowalczyk <majonezexe@protonmail.com>
2025-03-06 22:35:51 +0100

    Update Tailwind CSS to v4

--- app/layout.jsx
+++ app/layout.jsx
@@ -54,7 +54,7 @@ export default function RootLayout({ children }) {
  return (
   <html lang="en">
    <body className={GeistSans.className}>
-    <main className="flex min-h-screen flex-col items-center justify-center overflow-hidden bg-[#070706] bg-hero py-2 antialiased duration-200">
+    <main className="bg-hero flex min-h-screen flex-col items-center justify-center overflow-hidden bg-[#070706] py-2 antialiased duration-200">
      {children}
      <Toaster // prettier
       richColors={false}
