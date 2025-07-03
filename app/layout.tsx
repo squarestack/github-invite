b1493ab8befeca5414f18dc2c74c5c98aa1b8f52
Igor Kowalczyk <majonezexe@protonmail.com>
2025-07-03 13:14:43 +0200

    Format code

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,10 +12,12 @@ export const metadata: Metadata = {
   default: "Just.it",
   template: "%s | Just.it",
  },
- description: "We are building open source projects with strong focus on user privacy. Our goal is to make the Internet more secure. In our applications everything is encrypted - in fact even we don't have access to users' data.",
+ description:
+  "We are building open source projects with strong focus on user privacy. Our goal is to make the Internet more secure. In our applications everything is encrypted - in fact even we don't have access to users' data.",
  openGraph: {
   title: "Just.it",
-  description: "We are building open source projects with strong focus on user privacy. Our goal is to make the Internet more secure. In our applications everything is encrypted - in fact even we don't have access to users' data.",
+  description:
+   "We are building open source projects with strong focus on user privacy. Our goal is to make the Internet more secure. In our applications everything is encrypted - in fact even we don't have access to users' data.",
   url: "/",
   siteName: "Just.it",
   images: [
@@ -62,7 +64,11 @@ export default function RootLayout({ children }) {
       icons={{
        loading: (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5 animate-spin text-white">
-         <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+         <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+         />
         </svg>
        ),
       }}
